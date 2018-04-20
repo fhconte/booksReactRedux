d1a6ca3e63c1bc95f7ad2cb0777dbcb46a2f8156
Flavio <fhconte@gmail.com>
2018-04-20 14:34:40 -0300

    Code comments

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,7 @@
+// Node Modules
 import React, { Component } from 'react';
 
+// Containers
 import BookList from '../containers/book-list';
 
 export default class App extends Component {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,7 @@
+// Node Modules
 import { combineReducers } from 'redux';
+
+// Reducers
 import BooksReducer from './reducer-books';
 
 const rootReducer = combineReducers({
