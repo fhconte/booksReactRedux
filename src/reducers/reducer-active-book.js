3f8ce4959e837d4440b4e3e80ff70565a91a79b0
Flavio <fhconte@gmail.com>
2018-04-23 09:47:10 -0300

    Just some comments

--- src/reducers/reducer-active-book.js
+++ src/reducers/reducer-active-book.js
@@ -1,6 +1,7 @@
 // State = Reducer state
 export default function(state = null, action) {
     switch(action.type) {
+        // Here we define the comapitble case to dispatch action
         case 'BOOK_SELECTED':
         return action.payload;
     }
