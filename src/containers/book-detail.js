c86b7f9c8c18c8098c2dc0fff3c3a2d28ba3a5ab
Flavio <fhconte@gmail.com>
2018-04-23 09:36:45 -0300

    Book pages

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -7,11 +7,11 @@ class BookDetail extends Component {
         if (!this.props.book) {
             return <div>Select a book to get started.</div>
         }
-        
+
         return (
             <div>
-                <h3>Details for: </h3>
-                <div>{this.props.book.title}</div>
+                <h3>{this.props.book.title}</h3>
+                <div>Pages: {this.props.book.pages}</div>
             </div>
         );
     }
